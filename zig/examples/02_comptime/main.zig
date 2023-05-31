7f84986f97b22dcb20e54db5ba9a63cf2de82cc6
Nebojsa Koturovic <contact@kotur.me>
2023-05-31 20:08:25 +0200

    Add zig comptime example

---
+++ zig/examples/02_comptime/main.zig
@@ -0,0 +1,44 @@
+// const std = @import("std");
+
+pub fn main() u8 {
+    const r = comptime f()[1];
+    return r[1];
+}
+
+const Node = struct {
+    next: ?*const Node = null,
+    name: []const u8,
+};
+
+fn f() [10][]const u8 {
+    const node_a = Node{
+        .name = "Node A",
+    };
+
+    const node_b = Node{
+        .next = &node_a,
+        .name = "Node B",
+    };
+
+    const node_c = Node{
+        .next = &node_b,
+        .name = "Node C",
+    };
+
+    const result = comptime blk: {
+        var str: [10][]const u8 = .{};
+        var i = 0;
+        var it: ?*const Node = &node_c;
+        inline while (it) |node| {
+            str[i] = node.name;
+            it = it.?.next;
+            i += 1;
+        }
+        break :blk str;
+    };
+
+    // inline for (result) |str| {
+    //@compileLog(str[0..]);
+    //}
+    return result;
+}
