b65187023bfc2e157d4e11a6c4559b160bfe0a0a
Nebojsa Koturovic <contact@kotur.me>
2024-06-17 23:25:47 +0200

    Change some naming conventions in cpp examples

--- cpp/04_interview_examples_clang/src/main.cpp
+++ cpp/04_interview_examples_clang/src/main.cpp
@@ -1,7 +1,7 @@
 import examples;
 
-#include <cstdlib>
 #include <algorithm>
+#include <cstdlib>
 
 #include <argparse/argparse.hpp>
 #include <fmt/core.h>
@@ -9,11 +9,8 @@ import examples;
 
 auto main(int argc, char *argv[]) -> int {
   auto arg_parser = argparse::ArgumentParser("run-example");
-  arg_parser.add_argument("-e", "--example").help("runs the specified example");
-  arg_parser.add_argument("-l", "--list")
-      .help("prints list of all examples")
-      .implicit_value(true)
-      .default_value(false);
+  arg_parser.add_argument("-r", "--run").help("runs the specified emexample");
+  arg_parser.add_argument("-l", "--list").help("prints list of all examples").flag();
 
   try {
     arg_parser.parse_args(argc, argv);
@@ -23,29 +20,30 @@ auto main(int argc, char *argv[]) -> int {
     return EXIT_FAILURE;
   }
 
-  // a helper struct for stroing example information
-  struct Example {
+  // a helper struct for storing information about examples
+  struct example_entry {
     using function_type = void (*)();
     std::string_view name;
     function_type example;
   };
 
   constexpr auto examples_list =
-      std::array{Example{.name = "read_lines", .example = examples::read_lines}};
+      std::array{example_entry{.name = "read_lines", .example = examples::read_lines}};
 
-  if (arg_parser.is_used("--list")) {
+  if (arg_parser["--list"] == true) {
     fmt::println("List of examples: ");
     for (auto &&[name, _] : examples_list) {
       fmt::println("  - {}", name);
     }
-  } else if (arg_parser.is_used("--example")) {
-    auto target_example_name = arg_parser.get("--example");
-    const auto *example = std::ranges::find(examples_list, target_example_name, &Example::name);
-    if (example == std::cend(examples_list)) {
+  } else if (arg_parser.is_used("--run")) {
+    auto target_example_name = arg_parser.get("--run");
+    const auto *example_entry_it =
+        std::ranges::find(examples_list, target_example_name, &example_entry::name);
+    if (example_entry_it == std::cend(examples_list)) {
       fmt::println(stderr, "Example '{}' doesn't exist", target_example_name);
       return EXIT_FAILURE;
     }
     fmt::println("Running '{}' example ...", target_example_name);
-    example->example();
+    example_entry_it->example();
   }
 }
